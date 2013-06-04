0780ef0e5e31252dfeb582bf744fd62383837850
MichelleMHey <michellemhey@gmail.com>
2013-06-04 11:58:40 -0500

    Extremely happy post article

--- test/integration/anonymous_user_posts_article_test.rb
+++ test/integration/anonymous_user_posts_article_test.rb
@@ -1,5 +1,14 @@
 require 'test_helper'
 
 class AnonymousUserPostsArticleTest < ActionDispatch::IntegrationTest
-
+  test "posting a new article" do
+    visit '/'
+    click_link 'Write Post'
+    fill_in 'Title', with: 'My First Post!!'
+    fill_in 'Body', with: "I don't have anything to say"
+    click_button 'Submit Post'
+    assert_include page.body, "Your post has now been published!"
+    visit '/posts'
+    assert_include page.body, 'My First Post!!'
+  end
 end
